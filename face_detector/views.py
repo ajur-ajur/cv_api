918822d30edeb9036696ce2a40bd84601df44c36
ajur-ajur <lazuardialdira.form@gmail.com>
2023-08-03 16:58:15 +0700

    added ROI

--- face_detector/views.py
+++ face_detector/views.py
@@ -21,20 +21,28 @@ def detect(request):
             image = g._grab_image(url=url)
 
         small_image = cv2.resize(image, (0, 0), fx=0.25, fy=0.25)
-        image_grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        face_locations = fr.face_locations(small_image)
+
+        for (top, right, bottom, left) in face_locations:
+            ROI = image[top+20:bottom+20, left+20:right+20]
+
+        image_grey = cv2.cvtColor(ROI, cv2.COLOR_BGR2GRAY)
         input_image = np.expand_dims(np.expand_dims(
             cv2.resize(image_grey, (48, 48)), -1), 0)
 
         predict_emotion = m.model_emotion.predict(input_image)
         predict_age = m.model_age.predict(input_image)
-        face_locations = fr.face_locations(small_image)
 
         print(predict_age[0])
 
         data.update({"faces": face_locations, "success": True,
                      "emotion": m.get_emotion(predict_emotion),
                      "age": m.get_age(predict_age[0]),
-                     "gender": m.get_gender(predict_age[1])
+                     "gender": m.get_gender(predict_age[1]),
+                     "top": top,
+                     "right": right,
+                     "left": left,
+                     "bottom": bottom,
                      })
 
     return JsonResponse(data)
